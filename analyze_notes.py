5353892d1f3ec3b0bb8ca268f6d9f7e0ee930056
cmt0726 <teigec@wwu.edu>
2022-03-13 18:46:00 -0700

    init

---
+++ analyze_notes.py
@@ -0,0 +1,45 @@
+
+from music21 import *
+import os
+import pickle
+
+notes = []
+path = "midi_files/"
+for dir in os.listdir(path):
+    num_midis = len(os.listdir(path + "/" + dir))
+    progress = 0
+    for file in os.listdir(path + "/" + dir)[0:10]: #if we use more than this, each epoch becomes HUGE
+        parsed_midi = converter.parse(path + dir + "/" + file)
+        notes_to_parse = None
+        parts = instrument.partitionByInstrument(parsed_midi)
+        try:
+            if parts: # file has instrument parts
+                notes_to_parse = parts.parts[1].recurse()
+                
+            else: # file has notes in a flat structure
+                notes_to_parse = parsed_midi.flat.notes
+            for element in notes_to_parse:
+                if isinstance(element, note.Note):     
+                    notes.append(str(element.pitch))
+                elif isinstance(element, chord.Chord):
+                    notes.append('.'.join(str(n) for n in element.normalOrder))
+        except:
+            if parts: # file has instrument parts
+                notes_to_parse = parts.parts[0].recurse()
+                
+            else: # file has notes in a flat structure
+                notes_to_parse = parsed_midi.flat.notes
+            for element in notes_to_parse:
+                if isinstance(element, note.Note):     
+                    notes.append(str(element.pitch))
+                elif isinstance(element, chord.Chord):
+                    notes.append('.'.join(str(n) for n in element.normalOrder))
+        progress += 1
+        if progress % 5 == 0:
+            print("Analyzed: ", progress, "/", num_midis)
+        
+
+print("Done analyzing Midis!")
+
+with open("notes.pickle", "wb") as f:
+    pickle.dump(notes, f, protocol=pickle.HIGHEST_PROTOCOL)
